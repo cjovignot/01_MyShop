f06aadd3403f4faaff4ce68ca71cbd72b49b5713
arthur <arthur.asselin@epitech.eu>
2023-02-27 11:51:08 +0100

    added test form to sign in

--- signin.php
+++ signin.php
@@ -1,1 +1,22 @@
-<?php 
+<!DOCTYPE HTML>
+<form action="login.php" method="post">
+
+<h2>LOGIN</h2>
+
+<?php if (isset($_GET['error'])) { ?>
+
+    <p class="error"><?php echo $_GET['error']; ?></p>
+
+<?php } ?>
+
+<label>User Name</label>
+
+<input type="text" name="uname" placeholder="User Name"><br>
+
+<label>Password</label>
+
+<input type="password" name="password" placeholder="Password"><br> 
+
+<button type="submit">Login</button>
+
+</form>
