79b8bd6bdab823afaad4d3ed8c37c4c74673f85c
Cosme Jovignot <cosme.jovignot@epitech.eu>
2023-02-28 12:13:47 +0100

    signin.php

--- signin.php
+++ signin.php
@@ -46,8 +46,10 @@ if (isset($pdo) && isset($_POST['uname']) && isset($_POST['password'])){
     function sign_in($pdo,$uname,$password){
             $query_identify = $pdo->query("SELECT * FROM users WHERE username = '$uname'"); 
             $resuser = $query_identify->fetch(PDO::FETCH_ASSOC);
+            $salt = uniqid(false);
+            $hash = crypt($_POST['password'], $salt);
         
-                    if ($resuser && $resuser['username'] == $uname && $password == $resuser['password']){
+                    if ($resuser && $resuser['username'] == $uname && $hash == $resuser['password']) {
                         echo "Bienvenue " . htmlspecialchars($resuser['uname']) . "!\n";
                             $_SESSION['iduser'] = $resuser['id'];
                             $_SESSION['uname'] = $resuser['username'];
@@ -61,8 +63,6 @@ if (isset($pdo) && isset($_POST['uname']) && isset($_POST['password'])){
                     } else {
                         echo "User name or password are incorrect or don't exist.\n";
                     }
-        
-            
 }
 ?>
 
