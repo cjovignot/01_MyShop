1a8bceeeff7b0c471e94aac9383c04876547e5b3
Sebastien Garcia <SebGarcia@MSI>
2023-03-01 15:18:10 +0100

    seb 15:18

--- admincreateproducts.php
+++ admincreateproducts.php
@@ -1,11 +1,25 @@
+<!DOCTYPE HTML>
+<head>
+        <link rel="stylesheet" href="">
+        <meta charset="UTF-8">
+        <title>Wankers by Epitech</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="stylesheet.css" rel="stylesheet">
+</head>
+
 <?php
+include_once("connect_db.php");
+session_start();
+?>
 
-include_once ("connect_db.php");
 
-
-
+  <?php            
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
 	$productname = $_POST["name"];
+	// gestion si name existe deja : pas utile pour le moment
+	//$chk_product = $pdo->prepare("SELECT * FROM products WHERE name=?");
+	//$chk_product->execute([$productname]);
+	//$product_verif = $chk_product->fetch();
 	$price = $_POST["price"];
 	$description = $_POST["description"];
 	
@@ -14,15 +28,43 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 	$picture_tmp_name = $_FILES['picture']['tmp_name'];
 	$picture_error = $_FILES['picture']['error'];
 	
+	if ($description == NULL) {
+		$errordescription = "Description is missing, please check your informations - "; 
+		$reload = true;
+	
+	}
+	
+	if ($productname == NULL) {
+		$errorname = "Name is missing, please check your informations - "; 
+		$reload = true;
+	
+	}
+	// gestion si name existe deja : pas utile pour le moment
+	// if ($product_verif) {
+	// 	// username found
+	// 	$doubleproduct = "Product already exists - ";
+	// 	$reload = true;
+	// }
+
 	if ($picture_error === UPLOAD_ERR_OK) {
 		 //Move uploaded file to desired directory
 		move_uploaded_file($picture_tmp_name, 'uploads/' . $picture_name);
 		$image_path = 'uploads/' . $picture_name;
-  } else {
+  	} else {
 		// Handle upload error
 		$image_path = null;
+	
 	}
-  
+
+	if ($image_path == NULL) {
+		$errorimage = "Wrong size or format";
+		$reload = true;
+
+		
+	}
+
+	if (!$reload){
+		header('Location: admin_products.php');
   //var_dump($image_path);
 	// Insert product into database
 	$createProduct = $pdo->prepare("INSERT INTO products (name, price, description, image_path) VALUES (:name, :price, :description, :image_path)");
@@ -31,38 +73,61 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 	$createProduct->bindParam(':description', $description);
 	$createProduct->bindParam(':image_path', $image_path);
 	$createProduct->execute();
-	header('Location: admin_products.php');
 	
+}
 }
 
 	?>
 <!DOCTYPE HTML>
+
+<body class="login">
+	
 <form action="admincreateproducts.php" method="post" enctype="multipart/form-data">
 
-<h2>CREATE PRODUCTS</h2>
+<h2 class="login_title">CREATE PRODUCTS</h2>
 
 <!-- //  if (isset($_GET['error'])) { 
 	// <p class="error"> echo $_GET['error'];</p>
 -->
-<label>Product Name</label>
+<label class="login_input_field">Product Name</label>
 
-<input type="text" name="name" placeholder="Product Name"><br>
+<div class="login_input_field"><input type="text" name="name" placeholder="Product Name" class="validate"><?php echo $errorname; ?></div><br>
 
-<label>Product Price</label>
+<label class="login_input_field">Product Price</label>
 
-<input type="int" name="price" placeholder="Product Price"><br>
+<div class="login_input_field"><input type="number" name="price" placeholder="Product Price" required class="validate"></div><br>
 
-<label>Description</label>
+<label class="login_input_field">Description</label>
 
-<input type="text" name="description" placeholder="description"><br> 
+<div class="login_input_field"><input type="text" name="description" placeholder="description" class="validate"><?php echo $errordescription; ?></div><br> 
 
-<label for="picture">Upload a Picture:</label>
+<label for="picture" class="login_input_field">Upload a Picture:</label>
 
-<input type="file" name="picture"><br> 
+<div class="login_input_field"><input type="file" name="picture" class="validate"><?php echo $errorimage; ?></div><br> 
 
 <button type="submit">ADD</button>
 
 
 </form>
 
+<form action="admin_products.php" method="post">
+                <div class="form_buttons">
+                    <button class="form_button" type="submit" name="cancel">Cancel</button>
+                </div>
+            </form>
+            </div>
 
+</div>  
+
+</body>
+<!-- END DISPLAY PANEL / ACTION --------------------------------------------------------- -->
+
+
+
+
+
+          
+
+</body>
+
+</html>
