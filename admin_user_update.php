3f73fef5eef98b9e35f7d0e619bbfd079c9bf443
arthur <arthur.asselin@epitech.eu>
2023-03-01 17:28:44 +0100

    shhhh

--- admin_user_update.php
+++ admin_user_update.php
@@ -2,9 +2,16 @@
 <?php
 include_once("connect_db.php");
 session_start();
+if (!isset($_SESSION['uname']) && $_SESSION['admin'] != 1) {
+
+    header('Location: index.php');
+
+}
 $new_username = $_POST['new_username'];
 $new_email = $_POST['new_email'];
 $new_password = $_POST['new_password'];
+$new_admin = $_POST['admin_check'];
+$_SESSION['idupdate'] = $_GET['id'];
 ?>
 <!-- NAV ADMIN --------------------------------------- -->
 <html>
@@ -14,8 +21,9 @@ $new_password = $_POST['new_password'];
     <div class="panel" >         
         <div class="user_array">
             <h2>USER INFORMATIONS UPDATE</h2>
+            <h4>User info will remain the same if untouched</h4>
             <?php
-                $id = $_GET['id'];
+                $id = $id = $_GET['id'];
                 $stmt = $pdo->query("SELECT * FROM users WHERE id = '$id'"); // fetch data
                 $rows = $stmt->fetchAll();
                 // var_dump($_GET);
@@ -26,21 +34,77 @@ $new_password = $_POST['new_password'];
                         $_SESSION['old_password'] = $row['password'];
                         $_SESSION['old_email'] = $row['email'];
                         $_SESSION['old_username'] = $row['username'];
+                        $_SESSION['adminstatus'] = $row['admin'];
                         // var_dump($_SESSION);
                         // var_dump($_SESSION['old_password']);
                     }
                 } else {
                     echo "No user found";
                 }
-            ?>
+          
+                function update_user($pdo, $new_username, $new_email, $new_password, $new_admin) {
+                    $id = $_POST['id'];
+                    $chk_username = $pdo->query("SELECT username FROM users WHERE username = '$new_username'");
+                    $verif_uname = $chk_username->fetch(PDO::FETCH_ASSOC);
+                 
+                    if ($new_username == NULL) {
+                        $new_username = $_SESSION['old_username'];
+                    } 
+
+                    if ($new_username == $verif_uname['username']){
+                        echo "User already exist";
+                        $func_return = false;  
+                        return $func_return;      
+                    }
+                 
+                    if ($new_email == NULL) {
+                      $new_email= $_SESSION['old_email'];
+                    }
+                    if ($new_admin == NULL) {
+                        $new_admin = $_SESSION['adminstatus'];
+                      }
+                    if ($new_password == NULL) {
+                        $new_password= $_SESSION['old_password'];
+                    } else {
+                        $salt = uniqid(false);
+                        $hash = crypt($new_password, $salt);
+                        $new_password= $hash;
+                    }
+
+                    
+                    //var_dump($_SESSION);
+                    $query = $pdo->query("UPDATE users SET username = '$new_username', email = '$new_email', password = '$new_password' , admin = '$new_admin'
+                    WHERE id = '$id'");
+                     
+                    $func_return = true;
+                    return $func_return;
+                    var_dump($func_return);
+                }
+                
             
-            <form class="admin_form" action="admin_user_update.php" method="post">
+                
+                if ($_SERVER["REQUEST_METHOD"] == "POST"){
+                $test = update_user($pdo, $new_username, $new_email, $new_password, $new_admin);
+               
+             }
+             if ($test){
+                header('Location: admin_users.php');
+             }
+           
+               
+              
+
+            ?>
+
+
+
+<form class="admin_form" action="admin_user_update.php?id=<?php echo $_GET["id"]; ?>" method="post">
                 <label class="update_form_field"></label>
                     <div class="display">
                         <?php echo "Old username";?>
                     </div>
                     <div class="input_admin_form">
-                        <input type="text" name="new_username" placeholder="<?php echo $user_username;?>" class="validate">
+                        <input type="text" name="new_username" placeholder="<?php echo $user_username;?>" class="validate"> <?php echo $user_exist;?>
                     </div>
 
                 <label class="update_form_field"></label>
@@ -65,7 +129,12 @@ $new_password = $_POST['new_password'];
                     <div class="input_admin_form">
                         <input type="password" name="password_check" placeholder="<?php echo "Confirm password";?>" class="validate">
                     </div>
-                <input type="hidden" name="id" value='<?php echo $_GET['id'];?>'>
+                <label class="update_form_field">is admin</label>
+                    <div class="input_admin_form">
+                        <input type="radio" name="admin_check" value="1" > <label class="update_form_field">Yes</label>
+                        <input type="radio" name="admin_check" value="0" > <label class="update_form_field">No</label>
+                    </div>
+                <input type="hidden" name="id" value='<?php echo $_SESSION['idupdate'];?>'>
                 <button class="form_button" type="submit">Update</button>
             </form>
             <form action="admin_users.php" method="post">
@@ -73,19 +142,6 @@ $new_password = $_POST['new_password'];
                     <button class="form_button" type="submit" name="cancel">Cancel</button>
                 </div>
             </form>
-
-            <?php
-                function update_user($pdo, $new_username, $new_email, $new_password) {
-                    $id = $_POST['id'];
-                    $update_user = $pdo->query("UPDATE users SET username = '$new_username', email = '$new_email', password = '$new_password'
-                    WHERE id = '$id'");
-                    if ($_POST['username'] == NULL) {
-                        $_POST['username'] = $user_username;
-                    }
-                }
-                update_user($pdo, $new_username, $new_email, $new_password);
-                header('Location:admin_users.php');
-            ?>
         </div>
     </div>  
 <!-- END DISPLAY PANEL / ACTION --------------------------------------------------------- -->
