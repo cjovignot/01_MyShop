9d67476f6d07498fdaaf914cdeb7c6a173a51b8c
Cosme Jovignot <cosme.jovignot@epitech.eu>
2023-03-02 10:17:27 +0100

    MAJ

--- create_page.php
+++ create_page.php
@@ -7,7 +7,7 @@ session_start();
 <?php
     create_product_page($pdo);
 
-    function create_product_page($pdo){
+    function create_product_page($pdo) {
         $count_product = $pdo->query("SELECT COUNT(*) FROM products");
         $nb_product = $count_product->fetch(PDO::FETCH_NUM);
         $total_product = $nb_product[0];
@@ -18,12 +18,10 @@ session_start();
         while ($page_product < $nb_page) {
             $page_product++;
             $var_offset += 7;
-            ?>
-                <form action="index.php" method="get">
-                    <input type="submit" name="page" value=<?php echo $page_product ?>>
-                    <input type="hidden" name="offset" value=<?php echo $var_offset ?>>
-                </form>
-            <?php
+            echo "<form action='index.php' method='get'>
+            <input type='submit' name='page' value=$page_product>
+            <input type='hidden' name='offset' value=$var_offset>
+            </form>";
         }
     }
 ?>
