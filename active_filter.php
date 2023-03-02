5e84668efc88518cee023564820d2e3bd9551ff3
Cosme Jovignot <cosme.jovignot@epitech.eu>
2023-03-02 17:18:38 +0100

    MAJ Cosme

--- active_filter.php
+++ active_filter.php
@@ -3,38 +3,35 @@
     <label id="filter_test_label" for="filter_test">FILTER BY</label>
 
     <form class="form_test" method="get">
-            <select name="collection" class="field">
-                <option value="0">-- Choose a collection --</option>
-                <option value='1'>Collection 1</option>
-                <option value='2'>Collection 2</option>
-                <option value='3'>Collection 3</option>
-            </select>
-            <select name="color" class="field">
-                <option value="">-- Choose a color --</option>
-                <option value="black">Black</option>
-                <option value="white">White</option>
-                <option value="yellow">Yellow</option>
-            </select>
-            <select name="category" class="field">
-                <option value="">-- Choose a category --</option>
-                <option value="action">Action</option>
-                <option value="chill">Chill</option>
-                <option value="trip">Trip</option>
-            </select>
-                <div id="range_test">
-                <label id="range_test_label" for="filter_test">Price range</label>
-                <input id="minV" type="range" class="min-price" name="min_range" value="50" min="50" max="10000" step="50">
-                <input id="maxV" type="range" class="max-price" name="max_range" value="10000" min="50" max="10000" step="50" onchange='if(this.value != 0) { this.form.submit(); }'>
-                
-                    <div id="price_content_test">
-                        <div>
-                            <p id="min-value">$50</p>
-                        </div>
-                        <div >
-                            <p id="max-value">$10000</p>
-                        </div>
+        <select name="category" class="field">
+            <option value="none">
+                <?php
+                    if ($_GET['category']) {
+                        echo $_GET['category'];
+                    } else {
+                        echo "-- Choose a category --";
+                    }
+                ?>
+            </option>
+            <option value="Action">Action</option>
+            <option value="Chill">Chill</option>
+            <option value="Trip">Trip</option>
+        </select>
+
+        <div id="range_test">
+            <label id="range_test_label" for="filter_test">Price range</label>
+            <input id="minV" type="range" class="min-price" name="min_range" value="50" min="50" max="10000" step="50">
+            <input id="maxV" type="range" class="max-price" name="max_range" value="10000" min="50" max="10000" step="50">
+                <div id="price_content_test">
+                    <div>
+                        <p id="min-value">$50</p>
+                    </div>
+                    <div >
+                        <p id="max-value">$10000</p>
                     </div>
                 </div>
+        </div>
+        <input type="submit" value="Filter">
     </form>
 
 
